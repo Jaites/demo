cb1d0fd1c94e79dd17809c611ccf7d6e655dcf4e
Jaites <jupadim.ac@gmail.com>
2020-08-18 16:35:57 +0100

    Second commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is for the icons.
+ * @author ifeka
+ *
+ */
 @SpringBootApplication
 public class DemoApplication {
 
